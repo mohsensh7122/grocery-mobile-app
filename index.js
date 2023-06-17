1eb098e03049ec632f024c75a373fd6742b63177
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-06-16 21:47:04 -0400

    Added an event listener to the button
    This event listener will capture the value that is typed inside the input element.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+const addButton = document.getElementById('add-button');
+let input = document.getElementById('input-field');
+
+addButton.addEventListener('click', function(){
+    console.log(input.value);
+})
