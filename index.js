93d89f4c1dd303682f9eacdbe674a326b9a30cff
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-06-18 06:08:25 -0400

    Added the stylings

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const addButton = document.getElementById('add-button');
 let input = document.getElementById('input-field');
 
 addButton.addEventListener('click', function(){
-    console.log(input.value);
+
+    let inputValue = input.value;
+    console.log(inputValue);
 })
